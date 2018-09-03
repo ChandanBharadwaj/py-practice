00bd9c531882c6dc2ae1af79bb4dcf918f40f3d5
Chandan Bharadwaj <sai.chandanbharadwajkolluru9@t-mobile.com>
2018-09-03 13:08:58 +0530

    if

---
+++ basic/if.py
@@ -0,0 +1,19 @@
+'''
+Created on 03-Sep-2018
+
+@author: Chandan Bharadwaj
+'''
+if None:
+    print("if")
+else:
+    print("else")
+
+# elif
+age = input("Enter age")
+age=int(age)
+if age>10:
+    print("if")
+elif age==10:
+    print("elif")
+else:
+    print("else")
